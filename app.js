1529a8e25feef6878a0584c78cc46455b0ee59b1
Abhishek <starkop688@gmail.com>
2022-12-17 23:36:38 +0530

    change no2 in origin policy

--- app.js
+++ app.js
@@ -81,12 +81,11 @@ app.use((req, res, next) => {
     next();
 });
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin: *');
     res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin,X-Requested-With,Content-Type,Accept,Authorization'
+        'Access-Control-Allow-Headers:Origin,X-Requested-With,Content-Type,Accept,Authorization,X-Auth-Token'
     );
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods: GET,POST,PATCH,DELETE');
     next();
 });
 
